b0a077f9b8e5d60f03bb67fa090635b9a2f5b709
Mohammed Almasry <mohammed.hussein.mahmoud@gmail.com>
2025-05-07 18:08:57 +0300

    initial spring scaffold commit

---
+++ src/test/java/org/training/springblogger/SpringBloggerApplicationTests.java
@@ -0,0 +1,13 @@
+package org.training.springblogger;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBloggerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
