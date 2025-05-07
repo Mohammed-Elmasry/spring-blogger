9dca9909c90defd3137bad1ba4a3386d097e6d89
Mohammed Almasry <mohammed.hussein.mahmoud@gmail.com>
2025-05-07 18:37:39 +0300

    create User entity

---
+++ src/main/java/org/training/springblogger/entities/User.java
@@ -0,0 +1,34 @@
+package org.training.springblogger.entities;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.NonNull;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.sql.Timestamp;
+
+@Entity
+@Table(name = "users")
+@Data
+@NoArgsConstructor
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true, nullable = false)
+    private String username;
+
+    private String password;
+
+    @Column(unique = true, nullable = false)
+    private String email;
+
+    @CreationTimestamp
+    private Timestamp createdAt;
+
+    @UpdateTimestamp
+    private Timestamp updatedAt;
+}
